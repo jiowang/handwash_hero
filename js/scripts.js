0933ca4de861ee2fef83241133c8a6362fb917c3
Jio Wang <jio.wang@outlook.com>
2021-02-08 16:36:03 -0500

    ip change

--- js/scripts.js
+++ js/scripts.js
@@ -90,7 +90,7 @@ svg.selectAll("text")
 
 var fresh = true 
 
-Leap.loop(host = '129.97.167.34', port = 6437, function(frame) {
+Leap.loop(host = '2620:101:f000:4804::34 129.97.167.34', port = 6437, function(frame) {
 
 	if (frame.pointables.length > 0){
 		console.log(frame)
